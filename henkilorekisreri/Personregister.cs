f05faff0d4e9a93a7176841abea640659990c4a9
Olli Korhonen <K1937@student.jamk.fi>
2016-02-10 11:03:20 +0200

    jotain

---
+++ henkilorekisreri/Personregister.cs
@@ -0,0 +1,81 @@
+﻿/* 
+ * Copyright (C)
+ * 
+ * This file is part of the Person register project.
+ * 
+ * Created: <<10.2.2016>> 
+ * Authors: <<Olli Korhonen>> 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace henkilorekisreri
+{
+    /// <summary>
+    /// This class holds person objects in a collection.
+    /// </summary>
+    class Personregister
+    {
+        /// <summary>
+        /// Person List stores person objects.
+        /// </summary>
+        private List<Person> persons;
+
+        /// <summary>
+        /// Default constructor initializes a collection
+        /// </summary>
+        public Personregister()
+        {
+            persons = new List<Person>();
+        }
+        /// <summary>
+        /// Add a new person to collection
+        /// </summary>
+        /// <param name="person">person to add</param>
+        public void AddPerson(Person person)
+        {
+            persons.Add(person);
+        }
+
+        /// <summary>
+        /// Returns a person to caller application
+        /// </summary>
+        /// <param name="index">person index in collection</param>
+        /// <returns>person object</returns>
+        public Person GetPerson(int index)
+        {
+            if (index < persons.Count)
+            {
+                return persons.ElementAt(index);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public Person FindPerson(string socialSecurityNumber)
+        {
+            foreach(Person person in persons)
+            {
+                // is sotus same...
+                if (socialSecurityNumber.Equals(person.SocialSecurityNumber))
+                {
+                    return person; //person found with that sotu
+                }
+            }
+            return null; // person with search sotu not found..
+        }
+
+        public void PrintData()
+        {
+            foreach(Person person in persons)
+            {
+                Console.WriteLine(person.ToString());
+            }
+        }
+    }
+}
---
+++ henkilorekisreri/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace henkilorekisreri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a register 
+            Personregister myFriends = new Personregister();
+
+            // create a few person
+            Person person1 = new Person();
+            person1.Firstname = "Matti";
+            person1.Lastname = "Mainio";
+            person1.SocialSecurityNumber = "1234";
+
+            Person person2 = new Person();
+            person2 = new Person { Firstname = "Kirsi", Lastname = "Kernel", SocialSecurityNumber = "123456456"};
+
+            // add person to collection
+            myFriends.AddPerson(person1);
+
+            myFriends.AddPerson(person2);
+
+            // get one person from register
+            Person person3 = myFriends.GetPerson(0);
+            if (person3 != null)
+            {
+                Console.WriteLine(person3.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Can't find person in that index");
+            }
+
+            string sotu = "123456456";
+            Console.WriteLine("finding person with sotu" + sotu);
+            Person person4 = myFriends.FindPerson(sotu);
+            if (person4 != null)
+            {
+                Console.WriteLine(person4.ToString());
+            }
+            else
+            {
+                Console.WriteLine("cant find person with that sotu");
+            }
+            myFriends.PrintData();
+        }
+    }
+}
